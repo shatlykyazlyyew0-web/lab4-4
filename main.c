b4ae01e49e826a466a59c55590f5c784937d4eaf
Shatlyk Yazlyyew <shatlykyazlyyew0@gmail.com>
2026-02-24 11:33:04 +0700

    лаб 4.3

--- main.c
+++ main.c
@@ -1,7 +1,32 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 
-int main()
-{
-    printf("Hello World!\n");
+int main() {
+    char str[] = "abcda,efge abcd.";
+    char word[1000];
+    int point = 0;
+
+    for (int i = 0; str[i] != '\0';) {
+        while (str[i] == '.' || str[i] == ',' || str[i] == ' ') {
+            i++;
+        }
+
+        int j = 0;
+        while (str[i] != ',' && str[i] != '.' && str[i] != ' ' && str[i] != '\0') {
+            word[j] = str[i];
+            j++;
+            i++;
+        }
+        word[j] = '\0';
+
+        if (j > 0) {
+            printf("%s \n", word);
+
+            if (word[0] == word[j - 1]) {
+                point++;
+            }
+        }
+    }
+
+    printf("Summ: %d\n", point);
     return 0;
 }
