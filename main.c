b71c18a2b56e74752cd3c96d5deadcc534400d09
Shatlyk Yazlyyew <shatlykyazlyyew0@gmail.com>
2026-02-24 11:31:38 +0700

    лаб 4.2

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("Hello World!\n");
-    return 0;
+#include<stdio.h>
+int main() {
+        char str_1[] = "12345";
+        int result = 0;
+        for (int i = 0; str_1[i] != '\0'; i++) {
+                if ((str_1[i] < '0') || (str_1[i] > '9'))
+                {
+                        printf("error!!!");
+                        return 1;
+                }
+                result = result * 10 + (str_1[i] - '0');
+        }
+        printf("%d", result);
+        return 0;
 }
