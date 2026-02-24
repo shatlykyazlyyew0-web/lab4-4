80c1d9fb0821eddeaadccea92455a12bcd6e619f
Shatlyk Yazlyyew <shatlykyazlyyew0@gmail.com>
2026-02-24 11:28:12 +0700

    лаб 4.1

--- main.c
+++ main.c
@@ -2,6 +2,28 @@
 
 int main()
 {
-    printf("Hello World!\n");
+    int flag = 1;
+    int point = 0;
+    char str[100] = "()()(())";
+    // test: "()()(())"  ")())(())" "())("
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '(') {
+            point += 1;
+        }
+        if (str[i] == ')') {
+            point -= 1;
+        }
+        if (point < 0) {
+            flag = 0;
+            break;
+        }
+    }
+    if (flag && point == 0) {
+        printf("Balanced");
+    }
+    else {
+        printf("not balanced");
+    }
     return 0;
 }
