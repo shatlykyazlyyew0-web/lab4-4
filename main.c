e670b47c20884e0b9a80b0c70be0ca7f36d51a70
Shatlyk Yazlyyew <shatlykyazlyyew0@gmail.com>
2026-03-24 11:13:13 +0700

    программа полностью корректна

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main()
     // int flag = 1;
     int point = 0;
     int f = 1;
-    char str[100] = "()(())";
+    char str[100] = ")(()(())";
     // test: "()()(())"  ")())(())" "())("
 
     for (int i = 0; f && str[i] != '\0'; i++) {
@@ -15,7 +15,7 @@ int main()
         if (str[i] == ')') {
             point -= 1;
         }
-        if (point < 0) {
+        if (point < 0 && f == 1) {
             f = 0;
             printf("%s", "here i am \n");
         }
