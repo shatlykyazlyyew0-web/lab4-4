012ed538435b5f6b9ebce0cddd996359c8734b0c
Shatlyk Yazlyyew <shatlykyazlyyew0@gmail.com>
2026-04-12 17:52:47 +0700

    внесли несколько изменений. тело программы перенесли в функцию.

--- main.c
+++ main.c
@@ -1,32 +1,44 @@
 ﻿#include <stdio.h>
 
-int main() {
-    char str[] = "abcda,efge abcd.";
-    char word[1000];
-    int point = 0;
+int similar_word(const char *str, int *result_w)
+{
+  char word[1000];
+  int point = 0;
 
-    for (int i = 0; str[i] != '\0';) {
-        while (str[i] == '.' || str[i] == ',' || str[i] == ' ') {
-            i++;
-        }
+  for (int i = 0; str[i] != '\0';) {
+  while (str[i] == '.' || str[i] == ',' || str[i] == ' ') {
+      i++;
+  }
 
-        int j = 0;
-        while (str[i] != ',' && str[i] != '.' && str[i] != ' ' && str[i] != '\0') {
-            word[j] = str[i];
-            j++;
-            i++;
-        }
-        word[j] = '\0';
+  int j = 0;
+  while (str[i] != ',' && str[i] != '.' && str[i] != ' ' && str[i] != '\0') {
+      word[j] = str[i];
+      j++;
+      i++;
+  }
+  word[j] = '\0';
 
-        if (j > 0) {
-            printf("%s \n", word);
+  if (j > 0) {
+      // printf("%s \n", word);
 
-            if (word[0] == word[j - 1]) {
-                point++;
-            }
-        }
+      if (word[0] == word[j - 1]) {
+        printf("%s \n", word);
+        point++;
+      }
     }
+  }
+  *result_w = point;
+  return 0;
+}
 
-    printf("Summ: %d\n", point);
-    return 0;
+int main() {
+
+  int result;
+
+  if (similar_word("abcda, aaaa, jfjfd kjkf.", &result) == 0)
+  {
+    printf("Result: %d", result);
+  }
+
+  return 0;
 }
