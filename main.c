e12b2840f978eafafe52120e25db89ecb4f904fa
Shatlyk Yazlyyew <shatlykyazlyyew0@gmail.com>
2026-05-26 05:42:42 +0700

    добавили тесты

--- main.c
+++ main.c
@@ -1,32 +1,39 @@
-﻿#include <stdio.h>
+﻿// Задача3: Дана не пустая последовательность непустых слов из латинских букв;
+// соседниеслова отделены друг от друга запятой или пробелом, за последним словам точка.
+// Определить количество слов, которыенвчинаются и оканчиваются одной и той же буквой
+
+#include <stdio.h>
 
 int similar_word(const char *str, int *result_w)
 {
   if(str == NULL || result_w == NULL)
   {
-    // printf("%s", "Error!!! Point is NULL.");
     return -1;
   }
   char word[1000];
   int point = 0;
 
-  for (int i = 0; str[i] != '\0';) {
-  while (str[i] == '.' || str[i] == ',' || str[i] == ' ') {
+  for (int i = 0; str[i] != '\0';)
+  {
+    while (str[i] == '.' || str[i] == ',' || str[i] == ' ')
+    {
       i++;
-  }
+    }
 
-  int j = 0;
-  while (str[i] != ',' && str[i] != '.' && str[i] != ' ' && str[i] != '\0') {
+    int j = 0;
+    while (str[i] != ',' && str[i] != '.' && str[i] != ' ' && str[i] != '\0')
+    {
       word[j] = str[i];
       j++;
       i++;
-  }
-  word[j] = '\0';
+    }
 
-  if (j > 0) {
-      // printf("%s \n", word);
+    word[j] = '\0';
 
-      if (word[0] == word[j - 1]) {
+    if (j > 0)
+    {
+      if (word[0] == word[j - 1])
+      {
         printf("%s \n", word);
         point++;
       }
@@ -36,11 +43,11 @@ int similar_word(const char *str, int *result_w)
   return 0;
 }
 
-int main() {
+int main(){
 
   int result;
 
-  if (similar_word("abcda, aaaa, jfjfd kjkf axa.", &result) == 0)
+  if (similar_word("aba.", &result) == 0)
   {
     printf("Result: %d", result);
   }else {
@@ -49,3 +56,12 @@ int main() {
 
   return 0;
 }
+
+
+// "aba."
+// "abc, def."
+// "a, b, c."
+// "aba bcb,cdc."
+// "AbA, aBa."
+// ""
+// " . , . "
