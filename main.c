31a955d69433289eb7393aefb430fd379730aa54
Shatlyk Yazlyyew <shatlykyazlyyew0@gmail.com>
2026-06-02 13:18:15 +0700

    добавили проверку переполнения

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ int str_to_number(const char *str, int *out_result)
 
   int i = 0;
   int sign = 1;
-  int result = 0;
+  long long result = 0;
   while (str[i] == ' ')
   {
     i++;
@@ -38,13 +38,27 @@ int str_to_number(const char *str, int *out_result)
     {
       return -1;
     }
-    result = result * 10 + (str[i] - '0');
+
+    int digit = str[i] - '0';
+
+    result = result * 10 + digit;
+
+    if(result > 2147483647)
+    {
+      return -1;
+    }
   }
+
   if (sign == -1)
   {
     result *= -1;
+    // *out_result = result;
+    // return 0;
+    if(result < -2147483648)
+    {
+      return -1;
+    }
     *out_result = result;
-    return 0;
   }
   else if (sign == 1)
        {
@@ -56,7 +70,7 @@ int str_to_number(const char *str, int *out_result)
 int main(){
 
   int str;
-  if (str_to_number("123", &str) == 0)
+  if (str_to_number("99", &str) == 0)
   {
     printf("Result: %d", str);
   }else{
